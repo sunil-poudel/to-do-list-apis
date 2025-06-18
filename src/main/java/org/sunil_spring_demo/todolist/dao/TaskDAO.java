5e3357713e0919a3460ff8dc74c5e3a2c7e03dbb
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-18 14:02:41 +0545

    create task dao

---
+++ src/main/java/org/sunil_spring_demo/todolist/dao/TaskDAO.java
@@ -0,0 +1,10 @@
+package org.sunil_spring_demo.todolist.dao;
+
+import org.sunil_spring_demo.todolist.entity.Task;
+
+import java.util.List;
+
+public interface TaskDAO {
+    List<Task> getAllTasks();
+    Task getTaskById(int id);
+}
