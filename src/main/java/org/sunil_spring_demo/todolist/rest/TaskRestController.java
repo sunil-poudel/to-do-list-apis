2d0599cd0f487fd0b10dd09bd0f6638ebdd39833
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-18 14:20:45 +0545

    GetMapping to get all tasks

---
+++ src/main/java/org/sunil_spring_demo/todolist/rest/TaskRestController.java
@@ -0,0 +1,25 @@
+package org.sunil_spring_demo.todolist.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.sunil_spring_demo.todolist.dao.TaskDAO;
+import org.sunil_spring_demo.todolist.entity.Task;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/apis")
+public class TaskRestController {
+    private TaskDAO taskDAO;
+    @Autowired
+    public TaskRestController(TaskDAO taskDAO){
+        this.taskDAO = taskDAO;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks(){
+        return taskDAO.getAllTasks();
+    }
+}
