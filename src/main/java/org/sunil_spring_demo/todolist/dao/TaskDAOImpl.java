92f8c5726d2cf6a342c1ef984867915d32d81173
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-18 14:07:55 +0545

    implement task dao to get task by id

---
+++ src/main/java/org/sunil_spring_demo/todolist/dao/TaskDAOImpl.java
@@ -0,0 +1,32 @@
+package org.sunil_spring_demo.todolist.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sunil_spring_demo.todolist.entity.Task;
+
+import java.util.List;
+
+@Repository
+public class TaskDAOImpl implements TaskDAO{
+    private EntityManager entityManager;
+
+    @Autowired
+    public TaskDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        TypedQuery<Task> query = entityManager.createQuery(
+                "FROM LibraryBook", Task.class
+        );
+        return query.getResultList();
+    }
+
+    @Override
+    public Task getTaskById(int id) {
+        return null;
+    }
+}
