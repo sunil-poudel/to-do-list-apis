7d1e43f4d2ad12f5dbc05063ef8cc2f1c57fb13d
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-18 13:51:03 +0545

    initial commit

---
+++ src/test/java/org/sunil_spring_demo/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunil_spring_demo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
