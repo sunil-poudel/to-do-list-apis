50287bf66e4195d16bb3b087d69ca170f48ac7cf
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-19 21:45:28 +0545

    comment out generated value to post id from frontend

--- src/main/java/org/sunil_spring_demo/todolist/entity/Task.java
+++ src/main/java/org/sunil_spring_demo/todolist/entity/Task.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 @Table(name="task")
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
 
